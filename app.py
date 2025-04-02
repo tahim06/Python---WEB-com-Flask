8d6ebcd57ecbca1fd7b282a574152e93e04dc52a
thiago <tgtahim@gmail.com>
2025-04-02 14:49:46 -0300

    ajuste no nome da lista diario para Registros

--- app.py
+++ app.py
@@ -3,31 +3,32 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 conteudos = []
-diario = []
-@app.route('/', methods = ["GET", "POST"])
-def principal(): 
-    if request.method == 'POST':
-        if request.form.get('conteudo'):
+registros = []
+# localhost:5000/
+@app.route('/', methods=["GET", "POST"])
+def principal():
+    if request.method == "POST":
+        if request.form.get("conteudo"):
             conteudos.append(request.form.get("conteudo"))
-
+            
     return render_template(
-        'index.html',
-        conteudos = conteudos
+        "index.html",
+        conteudos=conteudos
     )
 
-@app.route('/diário')
+@app.route('/diario', methods=["GET", "POST"])
 def diario():
-    if request.method == 'POST':
-        if request.form.get('aluno') and request.form.get('nota'):
-            aluno = request.form.get('aluno')
-            nota = request.form.get('nota')
-            diario.append(
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            aluno = request.form.get("aluno") 
+            nota = request.form.get("nota")
+            registros.append(
                 {
-                    "aluno":aluno,
-                    "nota":nota
+                    "aluno": aluno,
+                    "nota": nota
                 }
             )
     return render_template(
-        'sobre.html', 
-        diario=diario
+        "sobre.html",
+        registros=registros  
     )
